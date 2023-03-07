6a6cbd033123649796c8110c48c19c7f1eae0714
Laercio Souza <laercioigorps@gmail.com>
2023-03-07 18:26:38 -0300

    update main app component to fetch and list products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    // fetch data
+    const dataFetch = async () => {
+      const data = await (
+        await fetch(
+          "http://127.0.0.1:8000/products"
+        )
+      ).json();
+      
+      // set state when the data received
+      setData(data);
+      if(data){
+        console.log(data)
+      }
+      
+    };
+
+    dataFetch();
+  }, []);
+
+  const listItems = data ? data.map(
+    (product) => (
+      <div className='col-md-3' key={product.sku}>
+      <div className="card m-4">
+        <div className="card-body">
+          <div><input type='checkbox' className="delete-checkbox"></input></div>
+          <p className="card-text text-center mb-0">{product.sku}</p>
+          <p className="card-text text-center mb-0">{product.name}</p>
+          <p className="card-text text-center mb-0">{product.price} $</p>
+          <p className="card-text text-center mb-0">
+            {Object.keys(product.type_specific)[0]}: {product.type_specific[Object.keys(product.type_specific)[0]]}
+            </p>
+        </div>
+      </div>
+      </div>
+    )
+  ) : ''
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container">
+    <div className="row mt-2">
+      <div className="col-md-10"><h1 className="">Product List</h1></div>
+      <div className="col-md-2">
+        <input className="me-2" type="button" value="ADD"/>
+        <input type="button" value="MASS DELETE"/>
+      </div>
     </div>
-  );
+    <div className="row">{listItems}</div>
+    
+  </div>)
 }
 
 export default App;
