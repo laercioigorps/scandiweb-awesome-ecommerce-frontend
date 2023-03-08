6f21829e4b6a5f2ab79853f2872b37fe44222992
Laercio Souza <laercioigorps@gmail.com>
2023-03-08 16:32:15 -0300

    update ADD button to link to create-product page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [data, setData] = useState();
@@ -6,53 +7,55 @@ const Home = () => {
   useEffect(() => {
     // fetch data
     const dataFetch = async () => {
-      const data = await (
-        await fetch(
-          "http://127.0.0.1:8000/products"
-        )
-      ).json();
-      
+      const data = await (await fetch("http://127.0.0.1:8000/products")).json();
+
       // set state when the data received
       setData(data);
-      if(data){
-        console.log(data)
+      if (data) {
+        console.log(data);
       }
-      
     };
 
     dataFetch();
   }, []);
 
-  const listItems = data ? data.map(
-    (product) => (
-      <div className='col-md-3' key={product.sku}>
-      <div className="card m-4">
-        <div className="card-body">
-          <div><input type='checkbox' className="delete-checkbox"></input></div>
-          <p className="card-text text-center mb-0">{product.sku}</p>
-          <p className="card-text text-center mb-0">{product.name}</p>
-          <p className="card-text text-center mb-0">{product.price} $</p>
-          <p className="card-text text-center mb-0">
-            {Object.keys(product.type_specific)[0]}: {product.type_specific[Object.keys(product.type_specific)[0]]}
-            </p>
+  const listItems = data
+    ? data.map((product) => (
+        <div className="col-md-3" key={product.sku}>
+          <div className="card m-4">
+            <div className="card-body">
+              <div>
+                <input type="checkbox" className="delete-checkbox"></input>
+              </div>
+              <p className="card-text text-center mb-0">{product.sku}</p>
+              <p className="card-text text-center mb-0">{product.name}</p>
+              <p className="card-text text-center mb-0">{product.price} $</p>
+              <p className="card-text text-center mb-0">
+                {Object.keys(product.type_specific)[0]}:{" "}
+                {product.type_specific[Object.keys(product.type_specific)[0]]}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      </div>
-    )
-  ) : ''
+      ))
+    : "";
 
   return (
-  <div className="container">
-    <div className="row mt-2">
-      <div className="col-md-10"><h1 className="">Product List</h1></div>
-      <div className="col-md-2">
-        <input className="me-2" type="button" value="ADD"/>
-        <input type="button" value="MASS DELETE"/>
+    <div className="container">
+      <div className="row mt-2">
+        <div className="col-md-10">
+          <h1 className="">Product List</h1>
+        </div>
+        <div className="col-md-2">
+          <Link className="btn btn-primary me-2" to="/add-product">
+            ADD
+          </Link>
+          <input className="btn btn-danger" type="button" value="MASS DELETE" />
+        </div>
       </div>
+      <div className="row">{listItems}</div>
     </div>
-    <div className="row">{listItems}</div>
-    
-  </div>)
-}
+  );
+};
 
 export default Home;
