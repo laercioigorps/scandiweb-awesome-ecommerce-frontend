6d39b9f976423fa470783fc0abb07d7c9b823ef1
Laercio Souza <laercioigorps@gmail.com>
2023-03-08 13:33:50 -0300

    add basic page Product add in path /add-product

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from './pages/NotFound'
 import Home from './pages/Home';
+import ProductAdd from './pages/ProductAdd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route index element={<Home />} />
+      <Route path="/add-product" element={<ProductAdd />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
---
+++ src/pages/ProductAdd.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+const ProductAdd = () => {
+  
+
+
+  return (
+  <div className="container">
+    <div className="row mt-2">
+      <div className="col-md-10"><h1 className="">Product ADD</h1></div>
+      <div className="col-md-2">
+        <input className="me-2" type="button" value="SAVE"/>
+        <input type="button" value="CANCEL"/>
+      </div>
+    </div>
+    <div className="row"></div>
+    
+  </div>)
+}
+
+export default ProductAdd;
